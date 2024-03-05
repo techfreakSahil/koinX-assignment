326c211de99bd70ff0260220605fecedf627abd7
techfreakSahil <gm6504@myamu.ac.in>
2024-03-05 20:09:24 +0530

    Add custom text colors and gradient configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        nav: {
+          primary: "#0F1629",
+          button: "#2870EA",
+        },
+        section: {
+          head: "#0B1426",
+          rank: "#808A9D",
+          inc: "#14B079",
+          incbg: "#EBF9F4",
+          highlight: "#0052FE",
+          secondary: "#111827",
+          card: "#26375F",
+        },
+      },
+    },
   },
   plugins: [],
 };
